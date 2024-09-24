d73a13625a686222258ca6d1c243e190b9e86459
Greytiger23 <104874165+Greytiger23@users.noreply.github.com>
2024-09-24 10:59:42 +0200

    fixed main ans script

--- script.js
+++ script.js
@@ -1,28 +1,3 @@
-// filter js
-$(documnet).ready(function(){
-    $('.filter-item').click(function(){
-        const value = $(this).attr('data-filter');
-        if (value == 'all') {
-            $('.post-box').show('1000');
-        }
-        else{
-            $('.post-box').not('.' + value).hide('1000');
-            $('.post-box').filter('.' + value).show('1000');
-        }
-    });
-    // active to btn
-    $('.filter-item').click(function(){
-        $(this).addClass("active-filter").siblings().removeClass('active-filter');
-    });
-});
-
-// header change on scroll
-let header = documnet.querySelector('header');
-
-window.addEventListener('scroll', () => {
-    header.classList.toggle("shadow", window.scrollY > 0);
-});
-
 const forms = document.querySelector(".forms"),
       pwShowHide = document.querySelectorAll(".eye-icon"),
       links = document.querySelectorAll(".link");
